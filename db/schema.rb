47e7d064b359e4220305b529dfaca9eddfb7ab7e
Billy Wallace <wallace.billy1@gmail.com>
2024-04-09 08:29:39 -0600

    Create migration to update credit card number to a string due to integer column constraints

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_08_220031) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_09_142406) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -58,7 +58,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_08_220031) do
   end
 
   create_table "transactions", force: :cascade do |t|
-    t.integer "credit_card_number"
+    t.string "credit_card_number"
     t.string "credit_card_expiration_date"
     t.integer "result"
     t.bigint "invoice_id", null: false

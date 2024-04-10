8aca2b929886aed724484b292dc614887068f737
Billy Wallace <wallace.billy1@gmail.com>
2024-04-10 08:22:48 -0600

    Create additional associations between models

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,7 +1,11 @@
 class Customer < ApplicationRecord
   has_many :invoices
+  has_many :transactions, through: :invoices
+  has_many :invoice_items, through: :invoices
+  has_many :items, through: :invoice_items
+  has_many :merchants, through: :items
 
   def name
-    "#{self.first_name}  #{self.last_name}"
+    "#{self.first_name} #{self.last_name}"
   end
 end

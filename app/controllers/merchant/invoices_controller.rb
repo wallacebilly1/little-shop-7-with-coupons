e76095de25146246e662b1ed67cefcb0fb7561ea
lance butler <lancej2004@gmail.com>
2024-04-10 15:58:19 -0600

    Feat: US 14, displays all of the invoices with links to show

--- app/controllers/merchant/invoices_controller.rb
+++ app/controllers/merchant/invoices_controller.rb
@@ -1,5 +1,10 @@
 class Merchant::InvoicesController < ApplicationController 
   def index 
+    @merchant = Merchant.find(params[:merchant_id])
     @invoices = Invoice.all
+  end
+
+  def show
+
   end
 end

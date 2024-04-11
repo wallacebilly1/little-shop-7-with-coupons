daff991c9f8fe9a54f93c0885b277655e0cab6b9
Billy Wallace <wallace.billy1@gmail.com>
2024-04-11 16:52:05 -0600

    Update spec file so that all tests pass

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -29,7 +29,8 @@ RSpec.describe Invoice, type: :model do
     @invoice6 = create(:invoice, customer_id: @customer4.id)
     @invoice7 = create(:invoice, customer_id: @customer5.id)
     @invoice8 = create(:invoice, customer_id: @customer6.id)
-
+    @invoice9 = create(:invoice, customer: @customer1, created_at:  Time.utc(2004, 9, 13, 12, 0, 0) )
+    
     @invoice1_transactions = create_list(:transaction, 4, invoice: @invoice1)
     @invoice4_transactions = create_list(:transaction, 3, invoice: @invoice4)
     @invoice5_transactions = create_list(:transaction, 2, invoice: @invoice5)
@@ -63,7 +64,6 @@ RSpec.describe Invoice, type: :model do
     @invoice_item5 = create(:invoice_item, item_id: @item5.id, invoice_id: @invoice3.id, status: 1)
     @invoice_item6 = create(:invoice_item, item_id: @item5.id, invoice_id: @invoice3.id, status: 1)
 
-    @invoice1 = create(:invoice, customer: @customer1, created_at:  Time.utc(2004, 9, 13, 12, 0, 0) )
   end
 
   describe "class methods" do
@@ -75,7 +75,7 @@ RSpec.describe Invoice, type: :model do
   describe "instance methods" do
     describe ".format_date" do
       it "formats date day, month, year" do
-        expect(@invoice1.format_date).to eq("Monday, September 13, 2004")
+        expect(@invoice9.format_date).to eq("Monday, September 13, 2004")
       end
     end
   end

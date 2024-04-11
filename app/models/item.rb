01099daaefbb415de9b8b3108d076e183465a1c9
Mel Langhoff <mlanghoff@uwalumni.com>
2024-04-11 17:02:00 -0600

    Test:US11/spec for merchant items index to create new item

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,7 @@ class Item < ApplicationRecord
   has_many :invoices, through: :invoice_items
   has_many :transactions, through: :invoices
   has_many :customers, through: :invoices
+
+  enum status: { "enabled" => 0, 
+  "disabled" => 1 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # root "articles#index"
   resources :merchants, only: [] do
     resources :dashboard, module: "merchant", only: [:index]
-    resources :items, module: "merchant", only: [:index, :show, :update, :edit]
+    resources :items, module: "merchant", except: [:destroy]
     resources :invoices, module: "merchant", only: [:index, :show]
   end
 

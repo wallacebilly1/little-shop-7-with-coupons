3aefcc4e115c14e40675dc85fbea542ac79cb87f
Rodrigo Chavez <Rodrigo103004@gmail.com>
2024-04-11 00:43:20 -0600

    US 25 Complete

--- spec/features/admin/merchants/index_spec.rb
+++ spec/features/admin/merchants/index_spec.rb
@@ -2,10 +2,9 @@ require "rails_helper"
 
 RSpec.describe "Merchants Index Page " do
   before(:each) do
-    @merchants = create_list(:merchant, 3)
-    @merchant1 = @merchants[0]
-    @merchant2 = @merchants[1]
-    @merchant3 = @merchants[2]
+    @merchant1 = Merchant.create(name: "Amazon")
+    @merchant2 = Merchant.create(name: "Walmart")
+    @merchant3 = Merchant.create(name: "Target")
   end
 
   describe '#us 24' do
@@ -17,6 +16,19 @@ RSpec.describe "Merchants Index Page " do
       expect(page).to have_content(@merchant2.name)
       expect(page).to have_content(@merchant3.name)
      
+    end
+  end
+
+  describe '#us 25' do
+    it 'see a specific merchants show page ' do
+      visit admin_merchants_path
+      # When I click on the name of a merchant from the admin merchants index page (/admin/merchants),
+      expect(page).to have_link("#{@merchant1.name}")
+      click_on("#{@merchant1.name}")
+      # Then I am taken to that merchant's admin show page (/admin/merchants/:merchant_id)
+      expect(current_path).to eq(admin_merchant_path(@merchant1.id))
+      # And I see the name of that merchant
+      expect(page).to have_content("#{@merchant1.name}")
     end
   end
 end

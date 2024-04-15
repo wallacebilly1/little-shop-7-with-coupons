8696f6a961562edebaedf5daf37a37ac19c780e1
Billy Wallace <wallace.billy1@gmail.com>
2024-04-15 15:13:49 -0600

    Build AR query for top 5 merchants and add to view page

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -7,5 +7,13 @@ class Merchant < ApplicationRecord
 
   enum status: {"Enabled" => 0, "Disabled" => 1}
 
-  
+  def self.top_five_merchants
+    Merchant.joins(:transactions)
+            .where("result = 1")
+            .distinct
+            .select("merchants.*, sum(invoice_items.quantity*invoice_items.unit_price) as total_revenue")
+            .group(:id)
+            .order("total_revenue desc")
+            .limit(5)
+  end
 end

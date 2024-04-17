b11af04275e408732007d35f47af3aa12262dd91
Mel Langhoff <mlanghoff@uwalumni.com>
2024-04-16 22:28:19 -0600

    Test/feat: Feature test and view complete

--- spec/features/merchants/items/index_spec.rb
+++ spec/features/merchants/items/index_spec.rb
@@ -338,4 +338,86 @@ RSpec.describe "Merchant Items Index" do
       end
     end
   end
+
+  describe 'User story 13' do    
+    it 'has the date for most sales next to the top 5 items' do
+      merchant1 = create(:merchant)
+      merchant2 = create(:merchant, name: "Amazon")
+  
+      customers = create_list(:customer, 10)
+      customer1 = customers[0]
+      customer2 = customers[1]
+      customer3 = customers[2]
+      customer4 = customers[3]
+      customer5 = customers[4]
+      customer6 = customers[5]
+  
+      invoices = create_list(:invoice, 3, customer: customer1)
+      invoice1 = invoices[0]
+      invoice2 = invoices[1]
+      invoice3 = invoices[2]
+      invoice4 = create(:invoice, customer_id: customer2.id, created_at: Time.utc(2004, 9, 13, 12, 0, 0))
+      invoice5 = create(:invoice, customer_id: customer3.id, created_at: Time.utc(2006, 1, 12, 1, 0, 0))
+      invoice6 = create(:invoice, customer_id: customer4.id)
+      invoice7 = create(:invoice, customer_id: customer5.id)
+      invoice8 = create(:invoice, customer_id: customer6.id)
+  
+      invoice1_transactions = create_list(:transaction, 5, invoice: invoice1)
+      transaction1 = invoice1_transactions[0]
+      transaction2 = invoice1_transactions[1]
+      transaction3 = invoice1_transactions[2]
+      transaction4 = invoice1_transactions[3]
+      transaction5 = invoice1_transactions[4]
+  
+      invoice4_transactions = create_list(:transaction, 4, invoice: invoice4)
+      transaction6 = invoice4_transactions[0]
+      transaction7 = invoice4_transactions[1]
+      transaction8 = invoice4_transactions[2]
+      transaction9 = invoice4_transactions[3]
+  
+      invoice5_transactions = create_list(:transaction, 3, invoice: invoice5)
+      transaction10 = invoice5_transactions[0]
+      transaction11 = invoice5_transactions[1]
+      transaction12 = invoice4_transactions[2]
+  
+      invoice6_transactions = create_list(:transaction, 2, invoice: invoice6)
+      transaction13 = invoice6_transactions[0]
+      transaction14 = invoice6_transactions[1]
+  
+      invoice7_transactions = create_list(:transaction, 1, invoice: invoice7)
+      transaction15 = invoice7_transactions[0]
+     
+      transaction16 = create(:transaction, invoice_id: invoice2.id)
+      transaction17 = create(:transaction, invoice_id: invoice3.id)
+      transaction18 = create(:transaction, invoice_id: invoice6.id)
+      transaction19 = create(:transaction, invoice_id: invoice7.id)
+  
+      item1 = create(:item, unit_price: 1, merchant_id: merchant1.id)
+      item2 = create(:item, unit_price: 23, merchant_id: merchant1.id)
+      item3 = create(:item, unit_price: 100, merchant_id: merchant1.id)
+      item4 = create(:item, unit_price: 5, merchant_id: merchant1.id)
+      item5 = create(:item, unit_price: 12, merchant_id: merchant1.id)
+  
+      invoice_item1 = create(:invoice_item, item_id: item1.id, invoice_id: invoice1.id, status: 0, unit_price: 10, quantity: 10)
+      invoice_item2 = create(:invoice_item, item_id: item2.id, invoice_id: invoice1.id, status: 2, unit_price: 11, quantity: 10)
+      invoice_item3 = create(:invoice_item, item_id: item3.id, invoice_id: invoice2.id, status: 2, unit_price: 12, quantity: 10)
+      invoice_item4 = create(:invoice_item, item_id: item4.id, invoice_id: invoice4.id, status: 1, unit_price: 13, quantity: 10)
+      invoice_item5 = create(:invoice_item, item_id: item5.id, invoice_id: invoice4.id, status: 1, unit_price: 14, quantity: 10)
+      invoice_item6 = create(:invoice_item, item_id: item5.id, invoice_id: invoice4.id, status: 1, unit_price: 15, quantity: 10)
+      invoice_item7 = create(:invoice_item, item_id: item5.id, invoice_id: invoice5.id, status: 1, unit_price: 16, quantity: 10)
+      invoice_item8 = create(:invoice_item, item_id: item5.id, invoice_id: invoice5.id, status: 1, unit_price: 17, quantity: 10)
+      invoice_item9 = create(:invoice_item, item_id: item5.id, invoice_id: invoice6.id, status: 1, unit_price: 18, quantity: 10)
+      invoice_item10 = create(:invoice_item, item_id: item5.id, invoice_id: invoice7.id, status: 1, unit_price: 19, quantity: 10)
+
+      visit merchant_items_path(merchant1)
+
+      within "#top-5-items" do
+        expect(page).to have_content("Top selling date for #{item1.name} was #{item1.top_selling_day}")
+        expect(page).to have_content("Top selling date for #{item2.name} was #{item2.top_selling_day}")
+        expect(page).to have_content("Top selling date for #{item3.name} was #{item3.top_selling_day}")
+        expect(page).to have_content("Top selling date for #{item4.name} was #{item4.top_selling_day}")
+        expect(page).to have_content("Top selling date for #{item5.name} was #{item5.top_selling_day}")
+      end
+    end
+  end
 end

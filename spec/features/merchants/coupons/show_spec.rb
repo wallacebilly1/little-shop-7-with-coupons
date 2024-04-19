9fb28d40b6820c6fdaf8a11c38726bb84273c509
Billy Wallace <wallace.billy1@gmail.com>
2024-04-19 14:46:18 -0600

    Create feature tests for Coupons US 3

---
+++ spec/features/merchants/coupons/show_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe "Merchant Coupons Show" do
+  before(:each) do
+    @merchant1 = create(:merchant)
+    @merchant2 = create(:merchant)
+    @coupon1 = @merchant1.coupons.create!(name: "50% off", code: "Half Off", disc_int: 50, disc_type: 0, status: 0)
+    @coupon2 = @merchant1.coupons.create!(name: "$5 off", code: "Take Five", disc_int: 5, disc_type: 1)
+    @coupon3 = @merchant1.coupons.create!(name: "$10 off", code: "Take Ten", disc_int: 10, disc_type: 1)
+    @coupon4 = @merchant2.coupons.create!(name: "10% off", code: "10-promo", disc_int: 10, disc_type: 0)
+
+    @customer1 = create(:customer)
+    @customer2 = create(:customer)
+    @customer3 = create(:customer)
+
+    @invoice1 = @coupon1.invoices.create!(customer_id: @customer1.id, status: 1)
+    @invoice2 = @coupon1.invoices.create!(customer_id: @customer2.id, status: 1)
+    @invoice3 = @coupon1.invoices.create!(customer_id: @customer3.id, status: 1)
+
+    @transaction1 = create(:transaction, invoice_id: @invoice1.id, result: 0)
+    @transaction2 = create(:transaction, invoice_id: @invoice2.id, result: 1)
+    @transaction3 = create(:transaction, invoice_id: @invoice3.id, result: 0)
+    
+    visit merchant_coupon_path(@merchant1, @coupon1) 
+  end
+
+  describe '#Coupons User Story 3' do
+    it "displays the name, code, status, discount amount/type, and number of times that a coupon has been used" do
+      expect(page).to have_content("Coupon Information")
+      expect(page).to have_content("Name: 50% Off")
+      expect(page).to have_content("Code: Half Off")
+      expect(page).to have_content("Discount: 50%")
+      expect(page).to have_content("Status: Active")
+      expect(page).to have_content("Number of Uses: 2")
+
+    end
+  end
+end

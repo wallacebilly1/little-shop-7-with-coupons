552d18b6e67ce9ccbd076335740154314272c97a
Billy Wallace <wallace.billy1@gmail.com>
2024-04-19 13:36:45 -0600

    Update and create views for new coupon page

--- app/controllers/merchant/coupons_controller.rb
+++ app/controllers/merchant/coupons_controller.rb
@@ -7,4 +7,25 @@ class Merchant::CouponsController < ApplicationController
     @merchant = Merchant.find(params[:merchant_id])
     @coupon = @merchant.coupons.find(params[:id])
   end
+
+  def new
+    @merchant = Merchant.find(params[:merchant_id])
+    @coupon = @merchant.coupons.new
+  end
+
+  def create
+    @merchant = Merchant.find(params[:merchant_id])
+    coupon = Coupon.new(coupon_params)
+    if coupon.save
+      redirect_to merchant_coupons_path(@merchant)
+    else
+      redirect_to new_merchant_coupon_path(@merchant)
+      flash[:notice] = "Failed to Create Coupon"
+    end
+  end
+
+  private
+  def coupon_params
+    params.permit(:name, :code, :disc_int, :disc_type, :merchant_id)
+  end
 end

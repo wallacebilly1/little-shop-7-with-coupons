c321aba375f647fb16bb1445e3c13f05ba4706ad
Billy Wallace <wallace.billy1@gmail.com>
2024-04-19 15:05:55 -0600

    Created functionality and AR query for Coupons US 3

--- spec/features/merchants/coupons/show_spec.rb
+++ spec/features/merchants/coupons/show_spec.rb
@@ -4,10 +4,10 @@ RSpec.describe "Merchant Coupons Show" do
   before(:each) do
     @merchant1 = create(:merchant)
     @merchant2 = create(:merchant)
-    @coupon1 = @merchant1.coupons.create!(name: "50% off", code: "Half Off", disc_int: 50, disc_type: 0, status: 0)
-    @coupon2 = @merchant1.coupons.create!(name: "$5 off", code: "Take Five", disc_int: 5, disc_type: 1)
-    @coupon3 = @merchant1.coupons.create!(name: "$10 off", code: "Take Ten", disc_int: 10, disc_type: 1)
-    @coupon4 = @merchant2.coupons.create!(name: "10% off", code: "10-promo", disc_int: 10, disc_type: 0)
+    @coupon1 = @merchant1.coupons.create!(name: "50% Off", code: "Half Off", disc_int: 50, disc_type: 0, status: 0)
+    @coupon2 = @merchant1.coupons.create!(name: "$5 Off", code: "Take Five", disc_int: 5, disc_type: 1)
+    @coupon3 = @merchant1.coupons.create!(name: "$10 Off", code: "Take Ten", disc_int: 10, disc_type: 1)
+    @coupon4 = @merchant2.coupons.create!(name: "10% Off", code: "10-promo", disc_int: 10, disc_type: 0)
 
     @customer1 = create(:customer)
     @customer2 = create(:customer)
@@ -16,10 +16,14 @@ RSpec.describe "Merchant Coupons Show" do
     @invoice1 = @coupon1.invoices.create!(customer_id: @customer1.id, status: 1)
     @invoice2 = @coupon1.invoices.create!(customer_id: @customer2.id, status: 1)
     @invoice3 = @coupon1.invoices.create!(customer_id: @customer3.id, status: 1)
+    @invoice4 = @coupon2.invoices.create!(customer_id: @customer3.id, status: 1)
+    @invoice5 = @coupon2.invoices.create!(customer_id: @customer3.id, status: 1)
 
     @transaction1 = create(:transaction, invoice_id: @invoice1.id, result: 0)
-    @transaction2 = create(:transaction, invoice_id: @invoice2.id, result: 1)
+    @transaction2 = create(:transaction, invoice_id: @invoice2.id, result: 0)
     @transaction3 = create(:transaction, invoice_id: @invoice3.id, result: 0)
+    @transaction4 = create(:transaction, invoice_id: @invoice4.id, result: 0)
+    @transaction5 = create(:transaction, invoice_id: @invoice5.id, result: 1)
     
     visit merchant_coupon_path(@merchant1, @coupon1) 
   end
@@ -31,8 +35,7 @@ RSpec.describe "Merchant Coupons Show" do
       expect(page).to have_content("Code: Half Off")
       expect(page).to have_content("Discount: 50%")
       expect(page).to have_content("Status: Active")
-      expect(page).to have_content("Number of Uses: 2")
-
+      expect(page).to have_content("Number of Uses: 3")
     end
   end
 end
--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -4,15 +4,32 @@ RSpec.describe Coupon, type: :model do
   before(:each) do
     @merchant1 = create(:merchant)
     @merchant2 = create(:merchant)
-    @coupon1 = @merchant1.coupons.create!(name: "50% off", code: "Half Off", disc_int: 50, disc_type: 0)
+    @coupon1 = @merchant1.coupons.create!(name: "50% off", code: "Half Off", disc_int: 50, disc_type: 0, status: 0)
     @coupon2 = @merchant1.coupons.create!(name: "$5 off", code: "Take Five", disc_int: 5, disc_type: 1)
     @coupon3 = @merchant1.coupons.create!(name: "$10 off", code: "Take Ten", disc_int: 10, disc_type: 1)
     @coupon4 = @merchant2.coupons.create!(name: "10% off", code: "10-promo", disc_int: 10, disc_type: 0)
+
+    @customer1 = create(:customer)
+    @customer2 = create(:customer)
+    @customer3 = create(:customer)
+
+    @invoice1 = @coupon1.invoices.create!(customer_id: @customer1.id, status: 1)
+    @invoice2 = @coupon1.invoices.create!(customer_id: @customer2.id, status: 1)
+    @invoice3 = @coupon1.invoices.create!(customer_id: @customer3.id, status: 1)
+    @invoice4 = @coupon2.invoices.create!(customer_id: @customer3.id, status: 1)
+    @invoice5 = @coupon2.invoices.create!(customer_id: @customer3.id, status: 1)
+
+    @transaction1 = create(:transaction, invoice_id: @invoice1.id, result: 0)
+    @transaction2 = create(:transaction, invoice_id: @invoice2.id, result: 0)
+    @transaction3 = create(:transaction, invoice_id: @invoice3.id, result: 0)
+    @transaction4 = create(:transaction, invoice_id: @invoice4.id, result: 0)
+    @transaction5 = create(:transaction, invoice_id: @invoice5.id, result: 1)
   end
 
   describe "relationships" do
     it { should belong_to(:merchant) }
     it { should have_many(:invoices)}
+    it { should have_many(:transactions)}
   end
 
   describe "validations" do
@@ -29,6 +46,13 @@ RSpec.describe Coupon, type: :model do
       it "combines the discount type and integer into a single printable value" do
         expect(@coupon1.formatted_disc).to eq "50%"
         expect(@coupon2.formatted_disc).to eq "$5"
+      end
+    end
+
+    describe ".succesful_uses_count" do
+      it "returns a count of how many successful transactions that a coupon has been used on" do
+        expect(@coupon1.successful_uses_count).to eq 3
+        expect(@coupon2.successful_uses_count).to eq 1
       end
     end
   end

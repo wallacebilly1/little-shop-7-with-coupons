6bd25cc030f1ecc84e005441ccf1b333d0cc47c3
Billy Wallace <wallace.billy1@gmail.com>
2024-04-20 09:20:00 -0600

    Create can_activate? instance method and model test

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -81,59 +81,61 @@ RSpec.describe Merchant, type: :model do
   end
 
   describe "class methods" do
-    it '#top_five_merchants' do
-      merchant1 = create(:merchant)
-      merchant2 = create(:merchant)
-      merchant3 = create(:merchant)
-      merchant4 = create(:merchant)
-      merchant5 = create(:merchant)
-      merchant6 = create(:merchant)
-      merchant7 = create(:merchant)
-      merchant8 = create(:merchant)
+    describe '#top_five_merchants' do
+      it "returns the top five merchants based on total revenue" do
+        merchant1 = create(:merchant)
+        merchant2 = create(:merchant)
+        merchant3 = create(:merchant)
+        merchant4 = create(:merchant)
+        merchant5 = create(:merchant)
+        merchant6 = create(:merchant)
+        merchant7 = create(:merchant)
+        merchant8 = create(:merchant)
 
-      invoice1 = create(:invoice)
-      invoice2 = create(:invoice)
-      invoice3 = create(:invoice)
-      invoice4 = create(:invoice)
-      invoice5 = create(:invoice)
-      invoice6 = create(:invoice)
-      invoice7 = create(:invoice)
-      invoice8 = create(:invoice)
+        invoice1 = create(:invoice)
+        invoice2 = create(:invoice)
+        invoice3 = create(:invoice)
+        invoice4 = create(:invoice)
+        invoice5 = create(:invoice)
+        invoice6 = create(:invoice)
+        invoice7 = create(:invoice)
+        invoice8 = create(:invoice)
 
-      item1 = create(:item, merchant:  merchant1)
-      item2 = create(:item, merchant:  merchant2)
-      item3 = create(:item, merchant:  merchant3)
-      item4 = create(:item, merchant:  merchant4)
-      item5 = create(:item, merchant:  merchant5)
-      item6 = create(:item, merchant:  merchant6)
-      item7 = create(:item, merchant:  merchant7)
-      item8 = create(:item, merchant:  merchant7)
+        item1 = create(:item, merchant:  merchant1)
+        item2 = create(:item, merchant:  merchant2)
+        item3 = create(:item, merchant:  merchant3)
+        item4 = create(:item, merchant:  merchant4)
+        item5 = create(:item, merchant:  merchant5)
+        item6 = create(:item, merchant:  merchant6)
+        item7 = create(:item, merchant:  merchant7)
+        item8 = create(:item, merchant:  merchant7)
 
-      create_list(:invoice_item, 5, unit_price: 1000, quantity: 5, invoice:  invoice1, item:  item1)
-      create_list(:invoice_item, 2, unit_price: 2000, quantity: 2, invoice:  invoice2, item:  item2)
-      create_list(:invoice_item, 1, unit_price: 5000, quantity: 2, invoice:  invoice3, item:  item3)
-      create_list(:invoice_item, 1, unit_price: 2000, quantity: 4, invoice:  invoice4, item:  item4)
-      create_list(:invoice_item, 4, unit_price: 4000, quantity: 5, invoice:  invoice5, item:  item5)
-      create_list(:invoice_item, 3, unit_price: 10000, quantity: 10, invoice:  invoice6, item:  item6)
-      create_list(:invoice_item, 2, unit_price: 9000, quantity: 1, invoice:  invoice7, item:  item7)
-      create_list(:invoice_item, 5, unit_price: 9000, quantity: 1, invoice:  invoice8, item:  item8)
+        create_list(:invoice_item, 5, unit_price: 1000, quantity: 5, invoice:  invoice1, item:  item1)
+        create_list(:invoice_item, 2, unit_price: 2000, quantity: 2, invoice:  invoice2, item:  item2)
+        create_list(:invoice_item, 1, unit_price: 5000, quantity: 2, invoice:  invoice3, item:  item3)
+        create_list(:invoice_item, 1, unit_price: 2000, quantity: 4, invoice:  invoice4, item:  item4)
+        create_list(:invoice_item, 4, unit_price: 4000, quantity: 5, invoice:  invoice5, item:  item5)
+        create_list(:invoice_item, 3, unit_price: 10000, quantity: 10, invoice:  invoice6, item:  item6)
+        create_list(:invoice_item, 2, unit_price: 9000, quantity: 1, invoice:  invoice7, item:  item7)
+        create_list(:invoice_item, 5, unit_price: 9000, quantity: 1, invoice:  invoice8, item:  item8)
 
-      create(:transaction, result: 1, invoice: invoice1)
-      create(:transaction, result: 1, invoice: invoice2)
-      create(:transaction, result: 0, invoice: invoice3)
-      create(:transaction, result: 1, invoice: invoice4)
-      create(:transaction, result: 1, invoice: invoice5)
-      create(:transaction, result: 1, invoice: invoice6)
-      create(:transaction, result: 1, invoice: invoice7)
-      create(:transaction, result: 1, invoice: invoice8)
-    
-      expect(Merchant.top_five_merchants).to match_array([ merchant6, merchant5,  merchant7, merchant1,  merchant2])
+        create(:transaction, result: 1, invoice: invoice1)
+        create(:transaction, result: 1, invoice: invoice2)
+        create(:transaction, result: 0, invoice: invoice3)
+        create(:transaction, result: 1, invoice: invoice4)
+        create(:transaction, result: 1, invoice: invoice5)
+        create(:transaction, result: 1, invoice: invoice6)
+        create(:transaction, result: 1, invoice: invoice7)
+        create(:transaction, result: 1, invoice: invoice8)
       
-      expect(Merchant.top_five_merchants[0].total_revenue).to eq 300000
-      expect(Merchant.top_five_merchants[1].total_revenue).to eq 80000
-      expect(Merchant.top_five_merchants[2].total_revenue).to eq 63000
-      expect(Merchant.top_five_merchants[3].total_revenue).to eq 25000
-      expect(Merchant.top_five_merchants[4].total_revenue).to eq 8000
+        expect(Merchant.top_five_merchants).to match_array([ merchant6, merchant5,  merchant7, merchant1,  merchant2])
+        
+        expect(Merchant.top_five_merchants[0].total_revenue).to eq 300000
+        expect(Merchant.top_five_merchants[1].total_revenue).to eq 80000
+        expect(Merchant.top_five_merchants[2].total_revenue).to eq 63000
+        expect(Merchant.top_five_merchants[3].total_revenue).to eq 25000
+        expect(Merchant.top_five_merchants[4].total_revenue).to eq 8000
+      end
     end
   end
 
@@ -155,6 +157,22 @@ RSpec.describe Merchant, type: :model do
         expected = [@item4, @item5]
 
         expect(@merchant1.packaged_items.uniq).to eq(expected)
+      end
+    end
+
+    describe "#can_activate?" do
+      it "returns a boolean for whether a coupon can be activated (aka if 5 active coupons already exist for that merchant)" do
+        @merchant3 = create(:merchant)
+        @coup1 = create(:coupon, merchant: @merchant3, status: 0)
+        @coup2 = create(:coupon, merchant: @merchant3, status: 0)
+        @coup3 = create(:coupon, merchant: @merchant3, status: 0)
+        @coup4 = create(:coupon, merchant: @merchant3, status: 0)
+
+        expect(@merchant3.can_activate?).to eq true
+
+        @coup5 = create(:coupon, merchant: @merchant3, status: 0)
+
+        expect(@merchant3.can_activate?).to eq false
       end
     end
   end
